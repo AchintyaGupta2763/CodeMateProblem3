fbe0793933f2912eb1fa7fd92c96053b12c5cbbd
Achintya Gupta <113601635+AchintyaGupta2763@users.noreply.github.com>
2025-09-20 23:39:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # config.py
-MONGODB_URI = "mongodb://localhost:27017"
+MONGODB_URI = "mongodb+srv://parkingsrm:ParkingSRM@parkingsrm.n8uqfya.mongodb.net/"
 DB_NAME = "deep_researcher"
 VECTOR_COLLECTION = "embeddings"
 EMBEDDING_DIM = 768  # adjust according to your embedding model
 TTL_SECONDS = 3600  # auto-delete after 1 hour
+
